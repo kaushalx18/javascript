ca0d57535b81b5f163a51b9bd4805f35387ff610
kaushalx18 <103758716+kaushalx18@users.noreply.github.com>
2024-04-08 10:10:58 +0000

    conversion of datatype

--- basics/datatype.js
+++ basics/datatype.js
@@ -5,10 +5,30 @@ console.log(3+3)
 
 
 let name="hitesh";//its become string
-let age = 19 // it is int
+//let age = 19 // it is int/number type
 let isloggedin = true;//it is boolean;
 let state;// it is undefined/null. 
 
 console.log(typeof undefined);//type of undefined is undefined
 console.log(typeof null);//but for null its object
 
+let age =undefined
+console.log(typeof age);
+
+let value = Number(age)//string convert to int/number
+console.log(typeof value);
+//console.log(value);//if age=19abc it will give NAN means not a number
+//console.log(value);//age=null it will give 0
+//console.log(value);//age=undefined then NaN
+//for boolean 0 and 1//
+
+let isloogedin=1;
+let bool=Boolean(isloogedin)
+console.log(bool);
+//for "" false
+//"kk"=true;
+
+let str = 33;
+let change = String(str);
+console.log(change);
+console.log(typeof change);
